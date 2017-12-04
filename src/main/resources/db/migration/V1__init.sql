5e241ec9b5aea3ec9c923784e5665911c1e03216
Cora Iberkleid <ciberkleid@pivotal.io>
2017-12-04 06:08:58 -0500

    Initial commit

---
+++ src/main/resources/db/migration/V1__init.sql
@@ -0,0 +1,4 @@
+CREATE TABLE fortune (
+    id BIGINT PRIMARY KEY AUTO_INCREMENT,
+    text varchar(255) not null
+);
---
+++ src/test/java/e2e/E2eTests.java
@@ -0,0 +1,51 @@
+package e2e;
+
+import org.assertj.core.api.BDDAssertions;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.ResponseEntity;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.web.client.RestTemplate;
+
+/**
+ * @author Marcin Grzejszczak
+ */
+@RunWith(SpringRunner.class)
+@SpringBootTest(classes = E2eTests.class,
+		webEnvironment = SpringBootTest.WebEnvironment.NONE)
+@EnableAutoConfiguration
+public class E2eTests {
+
+	// The app is running in CF but the tests are executed from Concourse worker,
+	// so the tests cannot retrieve the greeting-ui URL
+	// We will assume same host and replace app name
+	// (not compatible with c2c)
+
+	Logger logger = LoggerFactory
+			.getLogger(E2eTests.class);
+
+	@Value("${application.url}") String applicationUrl;
+
+	RestTemplate restTemplate = new RestTemplate();
+
+	@Test
+	public void should_return_a_fortune() {
+		ResponseEntity<String> response = this.restTemplate
+				.getForEntity("http://" + this.applicationUrl.replace("fortune-service", "greeting-ui") + "/", String.class);
+
+		logger.info("Response: [{}]", response);
+		BDDAssertions.then(response.getStatusCodeValue()).isEqualTo(200);
+
+		// Filter out the known Hystrix fallback responses
+		BDDAssertions.then(response.getBody()).doesNotContain("The fortuneteller will be back soon.").doesNotContain("This fortune is no good. Try another.");
+	}
+
+}
+
+
+
