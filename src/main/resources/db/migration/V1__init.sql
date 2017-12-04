34eec1df37fd05f04914c173a50ff6220ad6bf7b
Cora Iberkleid <ciberkleid@pivotal.io>
2017-12-04 10:50:41 -0800

    added insert statements to populate db

--- src/main/resources/db/migration/V1__init.sql
+++ src/main/resources/db/migration/V1__init.sql
@@ -1,6 +1,16 @@
+
 CREATE TABLE fortune (
     id BIGINT PRIMARY KEY AUTO_INCREMENT,
     text varchar(255) not null
 );
+
+INSERT INTO fortune (text) VALUES ('You learn from your mistakes... You will learn a lot today.');
+
+INSERT INTO fortune (text) VALUES ('You can always find happiness at work on Friday.');
+
+INSERT INTO fortune (text) VALUES ('You will be hungry again in one hour.');
+
+INSERT INTO fortune (text) VALUES ('Today will be an awesome day!');
+
 
 
