eab78e264519d7205d0ca56fc6256f5514c58400
Cora Iberkleid <ciberkleid@pivotal.io>
2017-12-04 17:57:50 -0800

    test classes from phase 2

---
+++ src/test/java/io/pivotal/FortuneServiceApplicationTests.java
@@ -0,0 +1,19 @@
+package io.pivotal;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import static org.junit.Assert.*;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest(classes = FortuneServiceApplication.class)
+public class FortuneServiceApplicationTests {
+
+    @Test
+    public void contextLoads() throws Exception {
+
+    }
+
+}
---
+++ src/test/java/smoke/SmokeTests.java
@@ -0,0 +1,43 @@
+package smoke;
+
+import org.assertj.core.api.BDDAssertions;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.ResponseEntity;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.web.client.RestTemplate;
+
+/**
+ * @author Marcin Grzejszczak
+ */
+@RunWith(SpringRunner.class)
+@SpringBootTest(classes = SmokeTests.class,
+        webEnvironment = SpringBootTest.WebEnvironment.NONE)
+@EnableAutoConfiguration
+public class SmokeTests {
+
+	@Value("${application.url}") String applicationUrl;
+
+	RestTemplate restTemplate = new RestTemplate();
+
+	@Test
+	public void should_return_a_fortune() {
+		ResponseEntity<String> response = this.restTemplate
+				.getForEntity("http://" + this.applicationUrl + "/", String.class);
+
+		BDDAssertions.then(response.getStatusCodeValue()).isEqualTo(200);
+
+		// Filter out the known Hystrix fallback response
+		BDDAssertions.then(response.getBody()).doesNotContain("The fortuneteller will be back soon.");
+	}
+
+}
+
+
+
+
+
+
