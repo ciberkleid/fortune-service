bdffc7923fe09d34540dcb44c1543ebdd79379ae
Cora Iberkleid <ciberkleid@pivotal.io>
2017-12-15 06:53:08 -0500

    more fortunes

--- src/main/resources/db/migration/V1__init.sql
+++ src/main/resources/db/migration/V1__init.sql
@@ -4,6 +4,12 @@ CREATE TABLE fortune (
     text varchar(255) not null
 );
 
+INSERT INTO fortune (text) VALUES ('Do what works.');
+
+INSERT INTO fortune (text) VALUES ('Do the right thing.');
+
+INSERT INTO fortune (text) VALUES ('Always be kind.');
+
 INSERT INTO fortune (text) VALUES ('You learn from your mistakes... You will learn a lot today.');
 
 INSERT INTO fortune (text) VALUES ('You can always find happiness at work on Friday.');
