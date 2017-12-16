0ce36c0f010c3dbccca1c1e625b74360aee499f8
Cora Iberkleid <ciberkleid@pivotal.io>
2017-12-16 15:42:06 -0500

    SQL correction for MySQL

--- src/main/java/io/pivotal/fortune/Fortune.java
+++ src/main/java/io/pivotal/fortune/Fortune.java
@@ -1,22 +1,15 @@
 package io.pivotal.fortune;
 
 import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
 
 @Entity
 public class Fortune {
 
     @Id
-    @GeneratedValue
     private Long id;
 
     private String text;
-
-    @Override
-    public String toString() {
-        return "Fortune [id=" + id + ", text=" + text + "]";
-    }
 
     public Long getId() {
         return id;
@@ -26,4 +19,8 @@ public class Fortune {
         return text;
     }
 
+    @Override
+    public String toString() {
+        return "Fortune [id=" + id + ", text=" + text + "]";
+    }
 }
