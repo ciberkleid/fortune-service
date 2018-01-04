7bca30fbcf077e5477cce3399f0df43c8a66045d
Cora Iberkleid <ciberkleid@pivotal.io>
2018-01-04 10:29:56 -0500

    cleanup

--- src/test/java/e2e/E2eTests.java
+++ src/test/java/e2e/E2eTests.java
@@ -3,8 +3,6 @@ package e2e;
 import org.assertj.core.api.BDDAssertions;
 import org.junit.Test;
 import org.junit.runner.RunWith;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -18,13 +16,9 @@ import org.springframework.web.client.RestTemplate;
 @EnableAutoConfiguration
 public class E2eTests {
 
-
 	// The app is running in CF but the tests are executed from Concourse worker,
 	// so the test will deduce the url to greeting-ui: it will assume the same host
 	// as fortune-service, and simply replace "fortune-service" with "greeting-ui" in the url
-
-	Logger logger = LoggerFactory
-			.getLogger(E2eTests.class);
 
 	@Value("${application.url}") String applicationUrl;
 
@@ -35,7 +29,6 @@ public class E2eTests {
 		ResponseEntity<String> response = this.restTemplate
 				.getForEntity("http://" + this.applicationUrl.replace("fortune-service", "greeting-ui") + "/", String.class);
 
-		logger.info("Response: [{}]", response);
 		BDDAssertions.then(response.getStatusCodeValue()).isEqualTo(200);
 
 		// Filter out the known Hystrix fallback responses from both fortune and greeting
