743efb0e2afaa2d19cd0b7d6df38757d6a5c9052
Cora Iberkleid <ciberkleid@pivotal.io>
2018-12-12 10:32:07 -0500

    lowercase table name - p.mysql tile compatibility

--- src/main/java/io/pivotal/fortune/FortuneRepository.java
+++ src/main/java/io/pivotal/fortune/FortuneRepository.java
@@ -6,7 +6,7 @@ import org.springframework.data.jpa.repository.Query;
 public interface FortuneRepository extends JpaRepository<Fortune, Long> {
 
     // This query is not efficient, but ok for such s small DB
-    @Query(value="SELECT * FROM FORTUNE ORDER BY RAND() LIMIT 1", nativeQuery = true)
+    @Query(value="SELECT * FROM fortune ORDER BY RAND() LIMIT 1", nativeQuery = true)
     Fortune findRandomFortune();
 
 }
