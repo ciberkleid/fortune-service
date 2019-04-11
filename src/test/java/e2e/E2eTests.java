1f7d10d47983699752695c5ddd3274b7620bea35
Cora Iberkleid <ciberkleid@pivotal.io>
2019-04-11 07:54:03 +0200

    removed dependency on greeting-ui

--- src/test/java/e2e/E2eTests.java
+++ src/test/java/e2e/E2eTests.java
@@ -27,12 +27,12 @@ public class E2eTests {
 	@Test
 	public void should_return_a_fortune() {
 		ResponseEntity<String> response = this.restTemplate
-				.getForEntity("http://" + this.applicationUrl.replace("fortune-service", "greeting-ui") + "/", String.class);
+				.getForEntity("http://" + this.applicationUrl + "/", String.class);
 
 		BDDAssertions.then(response.getStatusCodeValue()).isEqualTo(200);
 
-		// Filter out the known Hystrix fallback responses from both fortune and greeting
-		BDDAssertions.then(response.getBody()).doesNotContain("This fortune is no good. Try another.").doesNotContain("The fortuneteller will be back soon.");
+		// Filter out the known Hystrix fallback response
+		BDDAssertions.then(response.getBody()).doesNotContain("The fortuneteller will be back soon.");
 	}
 
 }
